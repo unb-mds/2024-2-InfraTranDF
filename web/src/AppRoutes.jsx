902a64e7995083177921fc9252de2418ba1eb982
Leonardo Sauma <141385833+leohssjr@users.noreply.github.com>
2024-12-09 19:07:45 -0300

    Update AppRoutes.jsx

--- web/src/AppRoutes.jsx
+++ web/src/AppRoutes.jsx
@@ -1,19 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./pages/App/App";
-import Login from "./pages/Login/Login";
-import Register  from "./pages/Register/Register"
+import Home from './pages/Home/Home';
+import Maps from './pages/Maps/Maps';
+import Report from './pages/Report/Report';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import { AuthProvider } from "./context/AuthContext";
 
 function AppRoutes() {
     return (
-
         <AuthProvider>
             <Routes>
-                <Route path="/" element={<App />}/>
-                <Route path="/Login" element={<Login />}/>
-                <Route path="/Register" element={<Register/>}/>
+                <Route path="/" element={<App />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/maps" element={<Maps />} />
+                <Route path="/report" element={<Report />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </AuthProvider>
-    )
+    );
 }
-export default AppRoutes
+
+
+export default AppRoutes;

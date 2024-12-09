20d7432eef4fe0ab2e62a6cee382851f2a45d8db
arturhk05 <arturhandow@gmail.com>
2024-12-08 23:03:58 -0300

    feat(backend): cria ambiente inicial para o backend e adiciona as dependecias: (express, axios, mysql2, unzipper)

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes/index.js'
+import connection from './config/db.js';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('Banco de dados funcionando se = 2:', rows[0].solution);
+});
+
+app.listen(3000, () => {
+    console.log(`Servidor rodando em http://localhost:3000`);
+});

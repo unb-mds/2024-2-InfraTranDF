09915b0c36976a372d5423dcd28182800652aa4d
GustavoGL <guga301104@gmail.com>
2024-12-09 23:33:59 -0300

    otimizando velocidade de processamento e remocao dos arquivos ao final do processo

--- backend/scraper/processaDados.js
+++ backend/scraper/processaDados.js
@@ -8,77 +8,63 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const csvDir = path.join(__dirname, 'extrato');
-
 const dadosBatch = [];
 
-const estadosParaRegioes = {
-    'ACRE': 'Norte',
-    'AMAPÁ': 'Norte',
-    'AMAZONAS': 'Norte',
-    'PARÁ': 'Norte',
-    'RONDÔNIA': 'Norte',
-    'RORAIMA': 'Norte',
-    'TOCANTINS': 'Norte',
-    'ALAGOAS': 'Nordeste',
-    'BAHIA': 'Nordeste',
-    'CEARÁ': 'Nordeste',
-    'MARANHÃO': 'Nordeste',
-    'PARAÍBA': 'Nordeste',
-    'PERNAMBUCO': 'Nordeste',
-    'PIAUÍ': 'Nordeste',
-    'RIO GRANDE DO NORTE': 'Nordeste',
-    'SERGIPE': 'Nordeste',
-    'DISTRITO FEDERAL': 'Centro-Oeste',
-    'GOIÁS': 'Centro-Oeste',
-    'MATO GROSSO': 'Centro-Oeste',
-    'MATO GROSSO DO SUL': 'Centro-Oeste',
-    'ESPÍRITO SANTO': 'Sudeste',
-    'MINAS GERAIS': 'Sudeste',
-    'RIO DE JANEIRO': 'Sudeste',
-    'SÃO PAULO': 'Sudeste',
-    'PARANÁ': 'Sul',
-    'RIO GRANDE DO SUL': 'Sul',
-    'SANTA CATARINA': 'Sul'
-};
-
 function obterRegiao(estado) {
+    const estadosParaRegioes = {
+        'ACRE': 'Norte',
+        'AMAPÁ': 'Norte',
+        'AMAZONAS': 'Norte',
+        'PARÁ': 'Norte',
+        'RONDÔNIA': 'Norte',
+        'RORAIMA': 'Norte',
+        'TOCANTINS': 'Norte',
+        'ALAGOAS': 'Nordeste',
+        'BAHIA': 'Nordeste',
+        'CEARÁ': 'Nordeste',
+        'MARANHÃO': 'Nordeste',
+        'PARAÍBA': 'Nordeste',
+        'PERNAMBUCO': 'Nordeste',
+        'PIAUÍ': 'Nordeste',
+        'RIO GRANDE DO NORTE': 'Nordeste',
+        'SERGIPE': 'Nordeste',
+        'DISTRITO FEDERAL': 'Centro-Oeste',
+        'GOIÁS': 'Centro-Oeste',
+        'MATO GROSSO': 'Centro-Oeste',
+        'MATO GROSSO DO SUL': 'Centro-Oeste',
+        'ESPÍRITO SANTO': 'Sudeste',
+        'MINAS GERAIS': 'Sudeste',
+        'RIO DE JANEIRO': 'Sudeste',
+        'SÃO PAULO': 'Sudeste',
+        'PARANÁ': 'Sul',
+        'RIO GRANDE DO SUL': 'Sul',
+        'SANTA CATARINA': 'Sul'
+    };
+
     return estadosParaRegioes[estado] || 'Desconhecida';
 }
 
-function extrairAno(dataPas) {
-    if (dataPas && dataPas.length >= 4) {
-        return parseInt(dataPas.substring(0, 4), 10); 
-    }
-    return null; 
-}
-
 function salvarNoBancoBatch(dadosBatch) {
-    const query = 'INSERT INTO focos (foco_id, lat, lon, data_pas, pais, estado, municipio, bioma, regiao, ano) VALUES ?';
-    connection.query(query, [dadosBatch], (err, results) => {
-        if (err) {
-            console.error('Erro ao salvar no banco de dados em massa:', err.message);
-            return;
-        }
-        console.log(`${dadosBatch.length} registros salvos com sucesso.`);
+    return new Promise((resolve, reject) => {
+        const query = 'INSERT INTO focos (foco_id, lat, lon, data_pas, pais, estado, municipio, bioma, regiao, ano) VALUES ?';
+        connection.query(query, [dadosBatch], (err) => {
+            if (err) {
+                console.error('Erro ao salvar no banco de dados em massa:', err.message);
+                return reject(err);
+            }
+            console.log(`${dadosBatch.length} registros salvos com sucesso.`);
+            resolve();
+        });
     });
 }
-fs.readdir(csvDir, (err, files) =>{
-    if (err) {
-        console.error('Erro ao leo o diretorio de CSVs:', err.message)
-        return;
-    }
 
-    const csvFiles = files.filter((file) => file.endsWith('.csv'))
-
-    csvFiles.forEach((file) =>{
-        const csvPath = path.join(csvDir, file)
-        console.log(`Processando arquivos CSV: ${csvPath}`)
-        
-        fs.createReadStream(csvPath)
+async function processarCSV(csvPath) {
+    console.log(`Processando arquivo: ${csvPath}`);
+    return new Promise((resolve, reject) => {
+        const stream = fs.createReadStream(csvPath)
             .pipe(csv())
             .on('data', (row) => {
                 const valores = [
-                    row.id_bdq,
                     row.foco_id,
                     parseFloat(row.lat),
                     parseFloat(row.lon),
@@ -88,44 +74,68 @@ fs.readdir(csvDir, (err, files) =>{
                     row.municipio,
                     row.bioma,
                     obterRegiao(row.estado),
-                    extrairAno(row.data_pas)
+                    parseInt(row.data_pas.slice(0, 4), 10)
                 ];
-                const valoresSemId = valores.slice(1);
 
-                if (valores.length === 11) {
-                    dadosBatch.push(valoresSemId);
+                if (valores.length === 10) {
+                    dadosBatch.push(valores);
 
-                    if (dadosBatch.length >= 1000) {
-                        salvarNoBancoBatch(dadosBatch);
-                        dadosBatch.length = 0;
+                    if (dadosBatch.length >= 17000) {
+                        stream.pause();
+                        salvarNoBancoBatch(dadosBatch)
+                            .then(() => {
+                                dadosBatch.length = 0;
+                                stream.resume();
+                            })
+                            .catch(reject);
                     }
                 }
             })
-            .on('end', () => {
+            .on('end', async () => {
                 if (dadosBatch.length > 0) {
-                    salvarNoBancoBatch(dadosBatch);
+                    await salvarNoBancoBatch(dadosBatch);
                 }
-                console.log('Arquivo CSV processado com sucesso.');
-
+                console.log(`Arquivo ${csvPath} processado com sucesso.`);
+                dadosBatch.length = 0;
+                resolve();
             })
             .on('error', (err) => {
-                console.error('Erro ao ler o arquivo CSV:', err.message);
+                console.error(`Erro ao processar o arquivo ${csvPath}:`, err.message);
+                reject(err);
             });
-    })
-})
-
-function excluirPasta(diretorio) {
-    fs.rm(diretorio, { recursive: true, force: true }, (err) => {
-        if (err) {
-            console.error('Erro ao excluir a pasta extrato:', err.message);
-        } else {
-            console.log('Pasta extrato excluída com sucesso.');
-        }
     });
 }
 
-process.on('exit', () => {
-    console.log('Excluindo a pasta extrato...');
-    excluirPasta(csvDir);
-    connection.end();
-});
+async function apagarArquivos() {
+    try {
+        const files = fs.readdirSync(csvDir);
+        for (const file of files) {
+            const filePath = path.join(csvDir, file);
+            fs.unlinkSync(filePath); // Remove o arquivo
+            console.log(`Arquivo ${file} apagado com sucesso.`);
+        }
+        console.log('Todos os arquivos foram apagados da pasta.');
+    } catch (err) {
+        console.error('Erro ao apagar arquivos:', err.message);
+    }
+}
+
+async function processarArquivosSequencialmente() {
+    try {
+        const files = fs.readdirSync(csvDir).filter((file) => file.endsWith('.csv'));
+
+        for (const file of files) {
+            const csvPath = path.join(csvDir, file);
+            await processarCSV(csvPath);
+        }
+
+        console.log('Todos os arquivos CSV foram processados.');
+        await apagarArquivos();
+    } catch (err) {
+        console.error('Erro ao processar os arquivos CSV:', err.message);
+    } finally {
+        connection.end();
+    }
+}
+
+processarArquivosSequencialmente();

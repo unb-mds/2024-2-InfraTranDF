4a17da22dda40485a3359ab5651ff25f9f55e250
arturhk05 <arturhandow@gmail.com>
2024-12-10 01:49:43 -0300

    feat(focus): cria um endpoint que retorna uma contagem de focos registrados com base no mes e ano passado

---
+++ backend/api/repositories/FocusRepository.js
@@ -0,0 +1,34 @@
+import connection from '../../config/db.js'
+
+export default class FocusRepository {
+
+    async getCountByYear(ano) {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT count(*) FROM focos WHERE ano = ?';
+            connection.query(query, [ano], (err, results) => {
+                if (err) {
+                    return reject(new Error('Erro ao obter dados: ' + err.message));
+                }
+                resolve(results);
+            });
+        });
+    }
+
+    async getMonthlyFocusByYear(month, year) {
+        const query = `
+            SELECT MONTH(data_pas) AS mes, estado, COUNT(*) AS contagem
+            FROM focos
+            WHERE MONTH(data_pas) = ? AND ano = ?
+            GROUP BY mes, estado;
+        `;
+
+        return new Promise((resolve, reject) => {
+            connection.query(query, [month, year], (err, results) => {
+                if (err) {
+                    reject(new Error('Erro ao obter dados: ' + err.message));
+                }
+                resolve(results);
+            });
+        });
+    }
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,9 +3,11 @@ import UserController from '../api/controllers/UserController.js';
 import AuthController from '../api/controllers/AuthController.js';
 import authenticateToken from '../api/middlewares/AuthMiddleware.js';
 import authenticateAdminToken from '../api/middlewares/AdminMiddleware.js';
+import FocusController from '../api/controllers/FocusController.js';
 
 const userController = new UserController();
 const authController = new AuthController();
+const focusController = new FocusController();
 
 const router = express.Router();
 
@@ -18,5 +20,7 @@ router.get('/users', authenticateAdminToken, userController.findAll)
 router.get('/users/:id', authenticateToken, userController.findById)
 
 router.post('/login', authController.authenticate)
+
+router.get('/focus/:month/:year', focusController.getMonthlyFocusData)
 
 export default router;

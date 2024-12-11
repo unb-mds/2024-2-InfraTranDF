562554dc24e8cd027fa4185b23d71f76c5fbc53a
arturhk05 <arturhandow@gmail.com>
2024-12-10 22:54:01 -0300

    Reapply "Merge branch 'main' of github.com:unb-mds/2024-2-InfraTranDF"
    This reverts commit d37b3a8befadbad9ce7956dcc0ccdd7602d229f7.

--- web/src/pages/Maps/Maps.jsx
+++ web/src/pages/Maps/Maps.jsx
@@ -13,7 +13,7 @@ const Maps = () => {
             <header className="home-header">
                 <img src="/src/assets/logo-png.png" alt="ChamaControl" className="logo-image" />
                 <nav className="navigation">
-                    <a href="/home">Início</a>
+                    <a href="/">Início</a>
                     <a href="/maps" className="active">Consultar Mapa</a>
                     <a href="/about">Equipe</a>
                 </nav>
